2281cc814616fb298c021de65103d113c9561d32
rifkimaulana04 <lolipopmerah1923@gmail.com>
2024-06-08 01:51:53 +0700

    Create operator.py

---
+++ 03_Day_Operator/operator.py
@@ -0,0 +1,79 @@
+# Operator in Python
+
+print('Addition: ', 1 + 2)
+print('Subtraction: ', 2 - 1)
+print('Multiplication: ', 2 * 3)
+print ('Division: ', 4 / 2)                         
+print('Division: ', 6 / 2)
+print('Division: ', 7 / 2)
+print('Division without the remainder: ', 7 // 2)   
+print('Modulus: ', 3 % 2)                           
+print ('Division without the remainder: ', 7 // 3)
+print('Exponential: ', 3 ** 2)                   
+
+# Floating numbers
+print('Floating Number,PI', 3.14)
+print('Floating Number, gravity', 9.81)
+
+# Complex numbers
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex number: ',(1 + 1j) * (1-1j))
+
+# Declaring the variable at the top first
+
+a = 3 
+b = 2 
+
+# Arithmetic operations and assigning the result to a variable
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+print(total) 
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
+
+# Declaring values and organizing them together
+num_one = 3
+num_two = 4
+
+# Arithmetic operations
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+div = num_two / num_two
+remainder = num_two % num_one
+
+# Printing values with label
+print('total: ', total)
+print('difference: ', diff)
+print('product: ', product)
+print('division: ', div)
+print('remainder: ', remainder)
+
+
+# Calculating area of a circle
+radius = 10                                 # radius of a circle
+area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+print('Area of a circle:', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')
